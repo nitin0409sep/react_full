b166602d9b262eb87a1ad02e7529e2b57f61cb0e
nitin0409verma <nitin.verma@remotestate.com>
2024-08-17 15:44:44 +0530

    Added comment in custom-react folder

--- custom-react/customreact.js
+++ custom-react/customreact.js
@@ -2,10 +2,10 @@ function customrender(reactElement, mainContainer) {
   const domElement = document.createElement(reactElement.type);
   domElement.innerHTML = reactElement.children;
 
-  for (let prop in reactElement.props) {
-    // console.log(reactElement.props.prop); // undefiend, as it will search for props.prop not with key
-    domElement.setAttribute(prop, reactElement.props["prop"]);
-  }
+    for(let prop in reactElement.props){
+        //!`` console.log(reactElement.props.prop); // undefiend, as it will search for props.prop not with key
+        domElement.setAttribute(prop, reactElement.props['prop']); 
+    }
 
   mainContainer.append(domElement);
 }
